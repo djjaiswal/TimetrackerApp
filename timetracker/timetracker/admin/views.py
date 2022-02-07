a6e809d6a4c52be7b93493b3003c5cf778dce111
dheeraj jaiswal <dheeraj@dheerajs-MacBook-Pro.local>
2022-02-07 08:29:04 +0530

    updated code

--- timetracker/timetracker/admin/views.py
+++ timetracker/timetracker/admin/views.py
@@ -1,7 +1,5 @@
-from flask import render_template, Blueprint, redirect, url_for, abort, flash
-from wtforms import SelectField
-from wtforms.validators import DataRequired
-
+from flask import render_template, Blueprint, redirect, url_for, abort, flash, request
+from timetracker import db
 from timetracker.models import Employee, Department, Project
 from .forms import RegistrationForm, DepartmentForm,ProjectForm
 from flask_login import login_required, current_user
@@ -19,24 +17,28 @@ def check_admin():
 @admin.route("/register", methods=["GET", "POST"])
 def register():
     check_admin()
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        emp_id = form.emp_id.data
-        emp = Employee.query.filter_by(emp_id=emp_id).first()
+
+    if request.method == "POST":
+        empid = request.form.get("empid")
+        fname = request.form.get("fname")
+        lname = request.form.get("lname")
+        department = request.form.get("departments")
+        password = request.form.get("password")
+        email = request.form.get("email")
+        emp = Employee.query.filter_by(emp_id=empid).first()
         if emp:
-            flash(f" Employee ID {emp_id} already assigned to Employee {emp.first_name} {emp.last_name}")
+            flash(f" Employee ID {empid} already assigned to Employee {emp.first_name} {emp.last_name}")
             return redirect(url_for('admin.register'))
-        email = form.email.data
-        first_name = form.first_name.data
-        last_name = form.last_name.data
-        password = form.password.data
-        emp = Employee(email ,emp_id, first_name, last_name, password )
-        dep_id = int(form.department.data)
+
+
+        emp = Employee(email ,empid, fname, lname, password)
+        dep_id = int(department)
         emp.department =Department.query.get(dep_id)
         emp.save_to_db()
+        flash(f"Employee {empid} added succesfully ")
         return redirect(url_for('admin.list_employees'))
-
-    return render_template("admin/register.html", form=form, title='Register')
+    deps = Department.query.all()
+    return render_template("admin/register.html", departments=deps, title='Register')
 
 
 
@@ -44,29 +46,40 @@ def register():
 @login_required
 def admin_dashboard():
     check_admin()
-    return render_template("admin/dasboard.html",title=" Admin Dashborad")
+    deps = Department.query.all()
+    emps = Employee.query.all()
+    projs= Project.query.all()
+    return render_template("admin/dasboard.html", dep=len(deps),emp=len(emps),proj=len(projs),title=" Admin Dashborad")
 
 
 @admin.route("/departments", methods=["GET", "POST"])
 @login_required
 def list_departments():
     check_admin()
-    form = DepartmentForm()
-    if form.validate_on_submit():
-        department = Department(name=form.name.data)
-        try:
-            department.save_to_db()
-            flash('You have successfully added a new department.')
-        except:
-            already_exist = Department.query.filter_by(name= form.name.data )
-            if already_exist:
-                flash('Error: Department Already exists.')
-            else:
-                flash('Oops: Something went wrong, Please try again.')
+    if request.method == "POST":
+        name = request.form.get("department-name")
+        existing = Department.query.filter_by(name = name).first()
+        if not existing:
+            newdep = Department(name=name)
+            newdep.save_to_db()
+        else:
+            flash(" Department already exist", "error")
         return redirect(url_for("admin.list_departments"))
     dep = Department.query.all()
-    return  render_template("admin/departments.html",departments = dep, form=form , title="Departments")
+    return  render_template("admin/departments.html",departments = dep, title="Departments")
 
+@admin.route("/departments/delete/<int:id>", methods=["GET", "POST"])
+@login_required
+def delete_departemnt(id):
+    if request.method == "POST":
+        department = Department.query.get(id)
+        for proj in department.projects:
+            project = Project.query.get(proj.id)
+            db.session.delete(project)
+            db.session.commit()
+        db.session.delete(department)
+        db.session.commit()
+    return redirect (url_for("admin.list_departments"))
 
 @admin.route("/departments/<int:id>", methods=["GET", "POST"])
 @login_required
@@ -74,14 +87,14 @@ def departmentpage(id):
     check_admin()
     department = Department.query.get(id)
     projects = Project.query.filter_by(department_belong_to=department).order_by(Project.date.desc()).all()
-    form = ProjectForm()
-    if form.validate_on_submit():
-        newproject = Project(name=form.name.data)
+
+    if request.method == "POST":
+        newproject = Project(name=request.form.get("name"))
         newproject.department_id = department.id
-        manager = Employee.query.filter_by(emp_id = form.manager.data).first()
+        manager = Employee.query.filter_by(emp_id = request.form.get("manager-id")).first()
 
         if not manager or manager.department_id != id:
-            flash(f'Employee with ID {form.manager.data} not found in this Department.')
+            flash(f'Employee with ID {request.form.get("manager-id")} not found in this Department.')
             return redirect(url_for("admin.departmentpage", id=id))
 
         newproject.project_manager = manager.emp_id
@@ -89,7 +102,7 @@ def departmentpage(id):
         try:
             manager.is_manager = True
             newproject.save_to_db()
-            flash('You have successfully added a new department.')
+            flash('You have successfully added a new department.' , "success")
         except:
             flash('Oops: Something went wrong, Please try again.')
 
@@ -97,8 +110,8 @@ def departmentpage(id):
     return render_template("admin/departmentpage.html",
                            department = department,
                            projects = projects,
-                           form = form,
                            title=department.name)
+
 
 
 @admin.route("/employees", methods=["GET", "POST"])
